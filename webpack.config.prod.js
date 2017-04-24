313e8cae70c0f002b0563d1785f21b1cf53f3437
Afaci <baumi494@gmail.com>
2017-04-24 19:09:13 +0200

    fixed deploy bug

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,22 +2,13 @@ let webpack = require('webpack');
 let baseConfig = Object.assign({}, require('./webpack.config.base'));
 
 baseConfig.devtool = "cheap-eval-source-map";
-baseConfig.plugins = baseConfig.plugins.concat([
-  new webpack.LoaderOptionsPlugin({
-    minimize: true,
-    debug: false
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    beautify: false,
-    comments: false
-  }),
-  new webpack.BannerPlugin({
-    banner: 'Ireland Blog  😃',
-    entryOnly: true
-  }),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': '"production"',
-  })
-]);
+
+baseConfig.plugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: '"production"'
+        }
+    })
+];
 
 module.exports = baseConfig;
